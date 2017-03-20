fcbcdea35e99ed487c9eef0f979092bc5a4fcd4f
ananda kumar hn <andu1989anand@gmail.com>
2017-03-20 13:23:27 +0530

    Simple Object Creation

---
+++ SimObj.java
@@ -0,0 +1,17 @@
+//Simple Object Creation
+public class SimObj {
+
+	int a;
+	
+	public static void main(String []args) {
+		
+		SimObj obj=new SimObj();
+		obj.a=90;
+System.out.println("anand "+(obj.a-10));
+
+	}
+
+}
+
+//OUTPUT:
+//anand 80

bf49cfd1398c83b2504b9f3e7919d4145ce13f9e
ananda kumar hn <andu1989anand@gmail.com>
2017-03-20 12:56:46 +0530

    Create name.java
    Reading name and age from keyboard

---
+++ name.java
@@ -0,0 +1,25 @@
+//Reading name and age from keyboard:
+import java.util.Scanner;
+class name
+{
+	public static void main(String [] args)
+	{
+		String name;
+		int age;
+		Scanner s=new Scanner(System.in);
+System.out.println("Enter your name:");
+		name=s.nextLine();
+System.out.println("Enter your age:");
+		age=s.nextInt();
+System.out.println("HAI "+name+", you are "+age+" years old");
+	}
+}
+
+/*
+OUTPUT:
+Enter your name:
+kumar
+Enter your age:
+30
+HAI kumar, you are 30 years old
+*/
